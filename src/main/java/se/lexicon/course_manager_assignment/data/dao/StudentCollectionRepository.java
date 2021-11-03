5f85470b864f6f1b89729539d7d99a92a47a25dd
BaselAskar <76973409+BaselAskar@users.noreply.github.com>
2021-11-03 10:44:50 +0100

    Finishing DAO

--- src/main/java/se/lexicon/course_manager_assignment/data/dao/StudentCollectionRepository.java
+++ src/main/java/se/lexicon/course_manager_assignment/data/dao/StudentCollectionRepository.java
@@ -4,8 +4,10 @@ package se.lexicon.course_manager_assignment.data.dao;
 
 import se.lexicon.course_manager_assignment.model.Student;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 
 
 public class StudentCollectionRepository implements StudentDao {
@@ -27,27 +29,43 @@ public class StudentCollectionRepository implements StudentDao {
 
     @Override
     public Student findByEmailIgnoreCase(String email) {
+        for (Student student : students){
+            if (student.getEmail().equalsIgnoreCase(email)) return student;
+        }
+
         return null;
     }
 
     @Override
     public Collection<Student> findByNameContains(String name) {
-        return null;
+        if (name.isEmpty() || name.equals(null)) throw new IllegalArgumentException("It's not allowed to be null or empty");
+
+        List<Student> studentsResult = new ArrayList<>();
+
+        for (Student student : students){
+            if (student.getName().equalsIgnoreCase(name)) studentsResult.add(student);
+        }
+
+        return studentsResult;
     }
 
     @Override
     public Student findById(int id) {
+        for (Student student : students){
+            if (student.getId() == id) return student;
+        }
+
         return null;
     }
 
     @Override
     public Collection<Student> findAll() {
-        return null;
+        return students;
     }
 
     @Override
     public boolean removeStudent(Student student) {
-        return false;
+        return students.remove(student);
     }
 
     @Override
